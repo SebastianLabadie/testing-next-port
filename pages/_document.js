ea02d1988e69a6596af607c3da9be24803369684
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-11-23 23:54:38 -0300

    per

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,9 @@ class MyDocument extends Document {
           <meta charset="utf-8" />
           <meta name="theme-color" content="#000000" />
           <meta
-            name="description"
-            content="Web site created using create-react-app"
+            name="Sebastian Labadie- Fullstack"
+            content="Portfolio created by Sebastian Labadie, made in nextjs."
           />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet" /> 
         </Head>
         <body>
           <Main />
