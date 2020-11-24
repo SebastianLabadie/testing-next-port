ab6bc47d925d687be00be80d7d5e2da3809b509b
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-11-24 00:38:07 -0300

    per2

--- pages/about.js
+++ pages/about.js
@@ -3,17 +3,17 @@ import Wrapper from "../src/components/Wrapper";
 import styled from "styled-components";
 import profileImg from "../assets/images/FotoCarne.jpg";
 import { motion } from "framer-motion";
-import LayoutInfoAndSkills from '../src/components/About/LayoutInfoAndSkills'
-import Head  from "next/head";
+import LayoutInfoAndSkills from "../src/components/About/LayoutInfoAndSkills";
+import Head from "next/head";
 import { useIntl } from "react-intl";
 
 const AboutStyled = styled.div`
-  min-width:100vw;
+  min-width: 100vw;
   margin: 0 1em;
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height:100vh;
+  min-height: 100vh;
   .ml {
     margin-left: 8px;
   }
@@ -25,8 +25,8 @@ const AboutStyled = styled.div`
     width: 100%;
     display: flex;
     img {
-      height: 100%;
-       width: 450px; 
+      height: 461px;
+      width: 450px;
       margin: auto;
       border-radius: 10px;
       border: none;
@@ -39,24 +39,24 @@ const AboutStyled = styled.div`
     width: 80%;
     height: 100%;
     justify-content: center;
-    margin:1em 0;
+    margin: 1em 0;
   }
-  
+
   @media screen and (max-width: 1280px) {
     .container {
       width: 90%;
       height: 92%;
-       img {
-        width:420px;
-    } 
+      img {
+        width: 420px;
+      }
+    }
   }
-}
   @media screen and (max-width: 940px) {
     margin: 0;
     .container {
       width: 100%;
       height: 80%;
-      img{
+      img {
         height: 85%;
       }
     }
@@ -64,33 +64,31 @@ const AboutStyled = styled.div`
   @media screen and (max-width: 768px) {
     .containerImg {
       height: 44%;
-      img{
-        height:88%;
-        width:350px;
+      img {
+        height: 88%;
+        width: 350px;
       }
     }
   }
   @media screen and (max-width: 578px) {
     .containerImg {
       height: 36%;
-      img{
-        height:88%;
-        width:310px;
+      img {
+        height: 88%;
+        width: 310px;
       }
     }
   }
-  
+
   @media screen and (max-width: 414px) {
-    align-items:flex-start;
-   
+    align-items: flex-start;
+
     .containerImg {
       height: 36%;
-      img{
-        width:270px;
+      img {
+        width: 270px;
       }
-      
     }
-    
   }
 `;
 
@@ -102,33 +100,32 @@ const pageVariants = {
   outX: {
     opacity: 0,
     x: "-90vw",
-  }
+  },
 };
 
-
 const About = () => {
-  const intl = useIntl()
+  const intl = useIntl();
   return (
     <>
       <Head>
         <title> Sebastian Labadie - {intl.messages["nav.about"]} </title>
       </Head>
-    <Wrapper>
-      <AboutStyled>
-        <motion.div 
-        initial={pageVariants.outX}
-        animate={pageVariants.inX}
-        exit={pageVariants.outX}
-        transition={{ duration: 1 }}
-        className="container">
-          <div className="containerImg">
-            <img src={profileImg} alt="sebastian labadie"   />
-          </div>
-          <LayoutInfoAndSkills />
-          
-        </motion.div>
-      </AboutStyled>
-    </Wrapper>
+      <Wrapper>
+        <AboutStyled>
+          <motion.div
+            initial={pageVariants.outX}
+            animate={pageVariants.inX}
+            exit={pageVariants.outX}
+            transition={{ duration: 1 }}
+            className="container"
+          >
+            <div className="containerImg">
+              <img src={profileImg} alt="sebastian labadie" />
+            </div>
+            <LayoutInfoAndSkills />
+          </motion.div>
+        </AboutStyled>
+      </Wrapper>
     </>
   );
 };
